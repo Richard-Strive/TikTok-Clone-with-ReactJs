ff58517cc688fca26fcf5ef55ea3734681d6a064
Richard-Strive <72205808+Richard-Strive@users.noreply.github.com>
2020-12-28 12:12:52 +0100

    first steps

---
+++ my-tiktok-clone/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCABItYvAMXX4sYiwSw4Q95CCnYFx4jKgg",
+  authDomain: "tiktok-8e762.firebaseapp.com",
+  projectId: "tiktok-8e762",
+  storageBucket: "tiktok-8e762.appspot.com",
+  messagingSenderId: "550812149433",
+  appId: "1:550812149433:web:99a02fed9754ce0a9edd68",
+};
---
+++ my-tiktok-clone/src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import Video from "./components";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello DevRichard, let's build tiktok clone!</h1>
+      <div className="app_videos">
+        <Video />
+      </div>
+    </div>
+  );
+}
+
+export default App;
