c62ec60da90bcd5186dd96ca16f2fd70c0d57dc2
Richard-Strive <72205808+Richard-Strive@users.noreply.github.com>
2020-12-28 12:36:18 +0100

    to continue App.js
    Devo completare i posizionamento di video

--- my-tiktok-clone/src/App.js
+++ my-tiktok-clone/src/App.js
@@ -1,11 +1,14 @@
 import "./App.css";
-import Video from "./components";
+import Video from "./components/Video";
 
 function App() {
   return (
     <div className="App">
       <h1>Hello DevRichard, let's build tiktok clone!</h1>
       <div className="app_videos">
+        <Video />
+        <Video />
+        <Video />
         <Video />
       </div>
     </div>
---
+++ my-tiktok-clone/src/components/Video.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import "../Video.css";
+export default function Video() {
+  return (
+    <div className="video">
+      <h1>This is a video</h1>
+    </div>
+  );
+}
